c82bee6a949209572474ab85148bda9cd2a1a421
Oliver Bains <oliver.bains@btinternet.com>
2020-07-19 11:05:41 +0200

    color update

--- src/flora-website/style/theme.js
+++ src/flora-website/style/theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 export default createMuiTheme({
   palette: {
     primary: {
-      main: "#ffffff"
+      main: "#2E5489"
     },
     secondary: {
       main: "#232324"
